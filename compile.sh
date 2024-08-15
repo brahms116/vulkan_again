6239317e02230547c58bae219c24aa9b1237a27f
David Kwong <20544dk@gmail.com>
2024-08-16 08:25:03 +1000

    Add shaders and compile script for first triangle

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+/usr/local/bin/glslc ./shaders/simple_shader.vert -o ./shaders/simple_shader.vert.spv
+/usr/local/bin/glslc ./shaders/simple_shader.frag -o ./shaders/simple_shader.frag.spv
