0d0e01b13eb99a321b09f78c6315cd165c107035
David Kwong <20544dk@gmail.com>
2024-09-08 11:23:01 +1000

    Unique ptr ubo

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -2,11 +2,18 @@
 
 #include "keyboard_movement_controller.hpp"
 #include "simple_render_system.hpp"
+#include "va_buffer.hpp"
 #include "va_model.hpp"
 
 #include <chrono>
 
 namespace va {
+
+struct GlobalUbo {
+  glm::mat4 projectionView;
+  glm::vec4 lightDirection =
+      glm::vec4(glm::normalize(glm::vec3(1.f, -3.f, -2.f)), 0.0f);
+};
 
 FirstApp::FirstApp() { loadGameObjects(); }
 
@@ -14,7 +21,8 @@ FirstApp::~FirstApp() {}
 
 void FirstApp::loadGameObjects() {
 
-  std::shared_ptr<VaModel> cubeModel = VaModel::createModelFromFile(vaDevice, "models/smooth_vase.obj");
+  std::shared_ptr<VaModel> cubeModel =
+      VaModel::createModelFromFile(vaDevice, "models/smooth_vase.obj");
 
   auto thing = VaGameObject::create();
   thing.model = cubeModel;
@@ -26,6 +34,19 @@ void FirstApp::loadGameObjects() {
 void FirstApp::run() {
   SimpleRenderSystem simpleRenderSystem{vaDevice,
                                         vaRenderer.getSwapChainRenderPass()};
+
+  std::vector<std::unique_ptr<VaBuffer>> uniformBuffers(
+      VaSwapChain::MAX_FRAMES_IN_FLIGHT);
+  for (int i = 0; i < uniformBuffers.size(); i++) {
+    uniformBuffers[i] = std::make_unique<VaBuffer>(
+        vaDevice, sizeof(GlobalUbo), 1, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        vaDevice.properties.limits.minUniformBufferOffsetAlignment);
+
+    uniformBuffers[i]->map();
+  }
+
   VaCamera camera{};
   auto cameraEmpty = VaGameObject::create();
   auto currentTime = std::chrono::high_resolution_clock::now();
@@ -51,6 +72,14 @@ void FirstApp::run() {
                       cameraEmpty.transform.rotation);
 
     if (auto commandBuffer = vaRenderer.beginFrame()) {
+      // Update
+      int frameIndex = vaRenderer.getFrameIndex();
+      GlobalUbo ubo{};
+      ubo.projectionView = camera.getProjection() * camera.getView();
+      uniformBuffers[frameIndex]->writeToBuffer(&ubo);
+      uniformBuffers[frameIndex]->flush();
+
+      // Render
       vaRenderer.beginSwapChainRenderPass(commandBuffer);
       simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
       vaRenderer.endSwapChainRenderPass(commandBuffer);
