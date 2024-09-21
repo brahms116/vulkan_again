ed4e64e9a55bd245830f71423868f5a6d3e8cf5d
David Kwong <20544dk@gmail.com>
2024-09-21 11:07:25 +1000

    Move the global ubo object

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -11,13 +11,6 @@
 
 namespace va {
 
-struct GlobalUbo {
-  glm::mat4 projectionMatrix;
-  glm::mat4 viewMatrix;
-  glm::vec4 ambientColor{1.f, 1.f, 1.f, .15f};
-  glm::vec4 lightPosition{-1.f, -1.f, -1.f, 1.f};
-  glm::vec4 lightColor{1.f};
-};
 
 FirstApp::FirstApp() {
   globalPool = VaDescriptorPool::Builder(vaDevice)
--- src/va_frame_info.hpp
+++ src/va_frame_info.hpp
@@ -7,6 +7,15 @@
 #include <vulkan/vulkan.h>
 
 namespace va {
+
+struct GlobalUbo {
+  glm::mat4 projectionMatrix;
+  glm::mat4 viewMatrix;
+  glm::vec4 ambientColor{1.f, 1.f, 1.f, .15f};
+  glm::vec4 lightPosition{-1.f, -1.f, -1.f, 1.f};
+  glm::vec4 lightColor{1.f};
+};
+
 struct FrameInfo {
   int frameIndex;
   float frameTime;
