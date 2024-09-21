d87d3b0ea3f825cc0d9c28da363146579e9c4811
David Kwong <20544dk@gmail.com>
2024-09-21 11:07:10 +1000

    Point light game object creation and component

--- src/va_game_object.cpp
+++ src/va_game_object.cpp
@@ -2,6 +2,16 @@
 
 namespace va {
 
+VaGameObject makePointLight(float intensity, float radius, glm::vec4 color) {
+  auto gameObject = VaGameObject::create();
+  gameObject.transform.scale.x = radius;
+  gameObject.pointLightComponent =
+      std::make_unique<PointLightComponent>(PointLightComponent{
+          .lightIntesity = intensity, .color = std::move(color)});
+
+  return gameObject;
+}
+
 // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
 /** Y X Z extrinsic rotation */
 glm::mat4 TransformComponent::mat4() const {
--- src/va_game_object.hpp
+++ src/va_game_object.hpp
@@ -18,6 +18,11 @@ struct TransformComponent {
   glm::mat4 normalMatrix() const;
 };
 
+struct PointLightComponent {
+  float lightIntesity{1.f};
+  glm::vec4 color{1.f, 1.f, 1.f, 1.f};
+};
+
 class VaGameObject {
 public:
   using id_t = int;
@@ -34,12 +39,15 @@ public:
     return VaGameObject(currentId++);
   }
 
+  static VaGameObject makePointLight(float intensity = 10.f, float radius = 0.f,
+                                     glm::vec4 color = glm::vec4(1.f));
+
   const id_t getId() { return id; };
 
-  std::shared_ptr<const VaModel> model{};
-
   TransformComponent transform{};
-  glm::vec4 color{};
+
+  std::shared_ptr<const VaModel> model{};
+  std::unique_ptr<const PointLightComponent> pointLightComponent = nullptr;
 
 private:
   id_t id;
