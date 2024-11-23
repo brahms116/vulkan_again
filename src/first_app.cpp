0a85c5da7a0046ccf4e0cad8bf1777d2263d6920
David Kwong <20544dk@gmail.com>
2024-11-23 12:32:42 +1000

    glm matrix is smoother

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -181,10 +181,10 @@ void FirstApp::run() {
       if (o.pointLightComponent != nullptr) {
         lightCamera.setViewTarget(o.transform.translation, {0.f, 0.f, 0.f});
 
-        /* ubo.lightProjectionMatrix = */
-        /*     glm::perspective(glm::radians(45.0f), 1.0f, 1.0f, 96.f); */
-        /* ubo.lightViewMatrix = glm::lookAt(o.transform.translation, */
-        /*                                   {0.f, 0.f, 0.f}, {0.f, 1.f, 0.f}); */
+        ubo.lightProjectionMatrix =
+            glm::perspective(glm::radians(45.0f), 1.0f, 1.0f, 96.f);
+        ubo.lightViewMatrix = glm::lookAt(o.transform.translation,
+                                          {0.f, 0.f, 0.f}, {0.f, 1.f, 0.f});
       }
     }
 
@@ -202,8 +202,8 @@ void FirstApp::run() {
       ubo.projectionMatrix = camera.getProjection();
       ubo.viewMatrix = camera.getView();
 
-      ubo.lightProjectionMatrix = lightCamera.getProjection();
-      ubo.lightViewMatrix = lightCamera.getView();
+      /* ubo.lightProjectionMatrix = lightCamera.getProjection(); */
+      /* ubo.lightViewMatrix = lightCamera.getView(); */
 
       ubo.inverseViewMatrix = cameraEmpty.transform.mat4();
       ubo.inverseViewMatrix = cameraEmpty.transform.mat4();
